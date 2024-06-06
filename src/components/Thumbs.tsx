e16e16ca1bbb763fa7437aa45ee782062ca0b443
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-06-06 00:57:03 -0500

    Actividad: Manejar el re-renderizado automático de Thumbs

--- src/components/Thumbs.tsx
+++ src/components/Thumbs.tsx
@@ -1,28 +1,28 @@
 import styles from './Thumbs.module.css';
-import { useState } from 'react';
-export default function Thumbs({product}){
+import { useState, useEffect } from 'react';
+export default function Thumbs({ product }) {
     const [thumb, setThumb] = useState(product.images[0] || "/mock1.jpg");
-
+    useEffect(() => setThumb(product.images[0]), [product.id]);
     return (
         <section className={styles["product-images-block"]}>
-        <div className={styles["product-images"]}>
-            {product.images.map(each =>
-            (<img
-                className={styles["mini-img"]}
-                key={each}
-                src={each}
-                alt={product.tittle}
-                onClick={() => setThumb(each)}
-            />)
-            )}
+            <div className={styles["product-images"]}>
+                {product.images.map(each =>
+                (<img
+                    className={styles["mini-img"]}
+                    key={each}
+                    src={each}
+                    alt={product.tittle}
+                    onClick={() => setThumb(each)}
+                />)
+                )}
 
-        </div>
-        <img
-            className={styles["big-img"]}
-            id="big-img"
-            src={thumb}
-            alt={product.tittle}
-        />
-    </section>
+            </div>
+            <img
+                className={styles["big-img"]}
+                id="big-img"
+                src={thumb}
+                alt={product.tittle}
+            />
+        </section>
     )
 }
